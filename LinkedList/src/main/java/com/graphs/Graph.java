a943086b1f4abf5973723fa2530a565e13a6891c
falseno9 <kunalmudgal@gmail.com>
2016-04-23 10:50:46 -0400

    Adding graph class

---
+++ LinkedList/src/main/java/com/graphs/Graph.java
@@ -0,0 +1,35 @@
+package com.graphs;
+
+/**
+ * Created by Kunal on 4/21/2016.
+ */
+public class Graph {
+    private int vertexCount;
+    private boolean[][] adjMatrix;
+    public Graph(int vertexCount) {
+        this.vertexCount = vertexCount;
+        adjMatrix = new boolean[vertexCount][vertexCount];
+    }
+
+    public void addEdge(int i, int j) {
+        if(i > 0 && i < vertexCount && j > 0 && j < vertexCount) {
+            adjMatrix[i][j] = true;
+            adjMatrix[j][i] = true;
+        }
+    }
+
+    public void removeEdge(int i, int j) {
+        if(i > 0 && i < vertexCount && j > 0 && j < vertexCount) {
+            adjMatrix[i][j] = false;
+            adjMatrix[j][i] = false;
+        }
+    }
+
+    public boolean isEdge(int i, int j) {
+        if(i > 0 && i < vertexCount && j > 0 && j < vertexCount) {
+            return adjMatrix[i][j];
+        } else {
+            return false;
+        }
+    }
+}
